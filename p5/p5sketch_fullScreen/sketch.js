2f9d311b322d11d39e66434842b9b60131811077
david-estrada <47006595+david-estrada@users.noreply.github.com>
2019-04-21 23:23:17 -0700

    david.estrada

--- p5/p5sketch_fullScreen/sketch.js
+++ p5/p5sketch_fullScreen/sketch.js
@@ -1,18 +1,22 @@
+
 let cat;
 
-
-
 function setup() {
-	// make canvas full screen
-	createCanvas(windowWidth, windowHeight);
-	cat = loadImage('../../p5_master/p5sketch_fullScreen/cat jumping.png');
+  // make canvas full screen
+  createCanvas(windowWidth, windowHeight);
 
+  cat = loadImage('cat.png');
+
+  noCursor();
 }
 
 
 function draw() {
-	// adding clear() to the draw loop will clear each frame, erasing object trails
-	// clear();
-	//fill(random(255));
-	ellipse(mouseX, mouseY, 100);
+  // adding clear() to the draw loop will clear each frame, erasing object trails
+  clear();
+
+
+  image(cat, mouseX - 150, mouseY - 100, cat.width / 3, cat.height / 3);
+  // fill(255, 50, 50);
+  // ellipse(mouseX, mouseY, 100);
 }
